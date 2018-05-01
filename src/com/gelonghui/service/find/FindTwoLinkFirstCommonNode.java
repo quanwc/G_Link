7bbe2745c5987ee304003c416c3d57db5ce02f6f
quanwenchao <quan_wc@163.com>
2018-05-01 23:48:36 +0800

    两个链表的公共节点，通过栈实现

--- src/com/gelonghui/service/find/FindTwoLinkFirstCommonNode.java
+++ src/com/gelonghui/service/find/FindTwoLinkFirstCommonNode.java
@@ -3,6 +3,8 @@ package com.gelonghui.service.find;
 import com.gelonghui.entity.Node;
 import com.gelonghui.util.NodeUtil;
 
+import java.util.*;
+
 /**
  * 输入两个单向链表，找出它们的第一个公共结点
  *
@@ -27,16 +29,46 @@ import com.gelonghui.util.NodeUtil;
  */
 public class FindTwoLinkFirstCommonNode {
 
-
     /**
      * 方式2：辅助栈
      * @param head1 第一个链表
      * @param head2 第二个链表
      * @return 第一个公共节点
      */
-    public static Node findFirstCommonNode1(Node head1, Node head2) {
+    public static Node findFirstCommonNode2(Node head1, Node head2) {
 
-        return null;
+        Deque<Node> stack1 = new LinkedList<Node>();
+        Deque<Node> stack2 = new LinkedList<Node>();
+
+        // 先将两个链表节点放入栈里
+        Node p1 = head1;
+        Node p2 = head2;
+        while (p1 != null) {
+            stack1.push(p1);
+            p1 = p1.getNext();
+        }
+        while (p2 != null) {
+            stack2.push(p2);
+            p2 = p2.getNext();
+        }
+
+        List<Node> list = new ArrayList<Node>();
+        // 出栈
+        while (stack1.size() >0 && stack2.size() > 0) {
+            Node node1 = stack1.pop();
+            Node node2 = stack2.pop();
+            if (node1 == node2) {
+                list.add(node1);
+            } else {
+                break;
+            }
+        }
+
+        Node result = null;
+        if (list.size() > 0) {
+            result = list.get(list.size()-1);
+        }
+        return result;
     }
 
     /**
@@ -45,7 +77,7 @@ public class FindTwoLinkFirstCommonNode {
      * @param head2 第二个链表
      * @return 第一个公共节点
      */
-    public static Node findFirstCommonNode2(Node head1, Node head2) {
+    public static Node findFirstCommonNode3(Node head1, Node head2) {
 
         // step1：得到两个链表的长度
         Integer length1 = getLinkLength(head1);
@@ -103,10 +135,57 @@ public class FindTwoLinkFirstCommonNode {
 
     public static void main(String[] args) {
 
-        //test1()
-        test1();
+        test2();
+
+        test3();
 
     }
+
+
+    /**
+     * 测试方法：
+     * 构建有公共节点的两个单向链表
+     *
+     * 公共节点为最后一个节点：
+     *  1 - 2 -3 - 4 \
+     *                 7
+     *         5 - 6 /
+     * @return
+     */
+    public static void test2() {
+
+        // step1、创建节点
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+
+        // step2、挂链
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node4);
+        node4.setNext(node7);
+        node5.setNext(node6);
+        node6.setNext(node7);
+        node7.setNext(null);
+
+        System.out.println("测试输出链表数据: ");
+        NodeUtil.printLink(node1);
+        NodeUtil.printLink(node5);
+        System.out.println("==============");
+
+        Node result = findFirstCommonNode2(node1, node5);
+        if (result == null) {
+            System.out.println("两链表没有公共节点");
+        } else {
+            System.out.println("公共节点为: " + result.getData());
+        }
+
+    }
+
 
     /**
      * 测试方法：
@@ -117,7 +196,7 @@ public class FindTwoLinkFirstCommonNode {
      *              6 - 7 - 8
      *      4 - 5 /
      */
-    public static void test1() {
+    public static void test3() {
         // step1、创建节点
         Node node1 = new Node(1);
         Node node2 = new Node(2);
@@ -138,28 +217,18 @@ public class FindTwoLinkFirstCommonNode {
         node7.setNext(node8);
         node8.setNext(null);
 
-        Node result = findFirstCommonNode2(node1, node4);
+
+        System.out.println("测试输出链表数据: ");
+        NodeUtil.printLink(node1);
+        NodeUtil.printLink(node4);
+        System.out.println("===================");
+
+        Node result = findFirstCommonNode3(node1, node4);
         if (result == null) {
             System.out.println("两链表没有公共节点");
         } else {
             System.out.println("公共节点为: " + result.getData());
         }
-    }
-
-
-    /**
-     * 测试方法：
-     * 构建有公共节点的两个单向链表
-     *
-     * 公共节点为最后一个节点：
-     *  1 - 2 -3 - 4 \
-     *                 7
-     *         5 - 6 /
-     * @return
-     */
-    public Node test2() {
-
-        return null;
     }
 
 }
