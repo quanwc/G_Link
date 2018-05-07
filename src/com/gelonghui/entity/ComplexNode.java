bd469aefea4ce50519f2162707f1a4885082758c
quanwenchao <quan_wc@163.com>
2018-05-07 19:50:54 +0800

    复杂链表复制

---
+++ src/com/gelonghui/entity/ComplexNode.java
@@ -0,0 +1,63 @@
+package com.gelonghui.entity;
+
+/**
+ * 复杂链表的节点类
+ * Created by quanwenchao
+ * 2018/5/6 19:22:38
+ */
+public class ComplexNode {
+    /**
+     * 数据域
+     */
+    private Integer data; // 以Integer为例，当然也可以用Object类型
+    /**
+     * next域
+     */
+    private ComplexNode next; // 用来指向下一个节点
+
+    private ComplexNode sibling; //兄弟姊妹节点，指向链表中的任一结点或者NULL
+
+    // 构造
+    public ComplexNode(Integer data) {
+        this.data = data;
+    }
+
+    public ComplexNode(ComplexNode next, ComplexNode sibling) {
+        this.next = next;
+        this.sibling = sibling;
+    }
+
+    // set、get
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+
+    public ComplexNode getNext() {
+        return next;
+    }
+
+    public void setNext(ComplexNode next) {
+        this.next = next;
+    }
+
+    public ComplexNode getSibling() {
+        return sibling;
+    }
+
+    public void setSibling(ComplexNode sibling) {
+        this.sibling = sibling;
+    }
+
+    @Override
+    public String toString() {
+        return "ComplexNode{" +
+                "data=" + data +
+                ", next=" + next +
+                ", sibling=" + sibling +
+                '}';
+    }
+}
--- src/com/gelonghui/service/clone/ComplexLinkCopy.java
+++ src/com/gelonghui/service/clone/ComplexLinkCopy.java
@@ -1,4 +1,10 @@
-package com.gelonghui.service.copy;
+package com.gelonghui.service.clone;
+
+import com.gelonghui.entity.ComplexNode;
+import com.gelonghui.entity.Node;
+import com.gelonghui.util.NodeUtil;
+
+import java.util.Optional;
 
 /**
  * 复杂链表的复制：
@@ -9,4 +15,18 @@ package com.gelonghui.service.copy;
  * 2018/5/4 15:42:24
  */
 public class ComplexLinkCopy {
+
+    /**
+     * 复杂链表的复制
+     * @param head 原链表
+     * @return 复制后的链表
+     */
+    public static ComplexNode copy1(ComplexNode head) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
